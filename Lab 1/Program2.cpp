17e61df45c7daa07eaf14592f186c84aa9add089
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2017-09-09 15:00:23 -0500

    Add files via upload

---
+++ Lab 1/Program2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{ 
+string n1 = "";
+string n2 = "";
+string n3 = "";
+
+float time1 = 0;
+float time2= 0;
+float time3 = 0;
+
+cout<< "please enter a name for the first runner: ";
+getline (cin, n1);
+
+cout<< "please enter a name for the second runner: ";
+getline (cin, n2);	
+	
+cout<< "please enter a name for the third runner: ";
+getline (cin, n3);	
+	
+cout << "please enter the time for the first runner: ";
+
+cout << "please enter the time for the second runner: ";
+	
+cout << "please enter the time for the third runner: ";	
+	return 0;
+}
